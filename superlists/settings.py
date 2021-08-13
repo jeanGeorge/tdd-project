66fe57a216a8509918bc5999ce63433442d6fab2
Jean George <jean.george@leanonsystems.com>
2021-08-13 19:54:52 -0300

    Modelo de usuário mínimo

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists'
+    'lists',
+    'accounts',
 ]
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
